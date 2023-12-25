5bde5f8dd6db496b5d979a78ec12cf3c5bd3c1e7
rezch <krudanya1008@gmail.com>
2023-12-25 17:49:02 +0300

    functions upd

--- barmo.py
+++ barmo.py
@@ -3,11 +3,13 @@ import sys
 REGISTER = None
 STACK = []
 QUEUE = []
+#STACK_BORDER = 0
 
 IN_GOTO = []
-SKIP = False
 
-INPUT_QUEUE = []
+FUNC_TABLE = {}
+IN_FUNC = False
+
 STEP = False
 
 OUTPUT_LOG = []
@@ -27,8 +29,16 @@ class CodeFileError(Exception):
     def __str__(self):
         if self.message:
             return 'CodeFileError, {}'.format(self.message)
-        else:
-            return 'CodeFileError, check code source file'
+        return 'CodeFileError, check code source file'
+        
+
+def InterpreterError(index=None, msg=None):
+    print('InterpreterError')
+    if index is not None:
+        print(f'at symbol: {index}\n')
+    if msg is not None:
+        print(f'Traceback: {msg}')
+    exit(1)
 
 
 def arg_parse(source=None):
@@ -71,8 +81,9 @@ def refactor(file_text):
     return file_text.replace('\n', '').replace('\t', '').replace(' ', '')
 
 
-def com(command, i):
-    global REGISTER, STACK, QUEUE, IN_GOTO, SKIP, INPUT_QUEUE
+def com(command, i, SKIP):
+    global REGISTER, STACK, QUEUE, IN_GOTO
+
     if command == '#':
         REGISTER *= 10
     elif command == '_':
@@ -84,16 +95,19 @@ def com(command, i):
         print(REGISTER, end='')
         REGISTER = None
     elif command == '?':
-        if INPUT_QUEUE:
-            REGISTER = INPUT_QUEUE.pop(0)
-        else:
-            REGISTER = None
+        REGISTER = int(input())
     elif command == '[':
-        REGISTER = STACK.pop()
+        if len(STACK) == 0:
+            REGISTER = None
+        else:
+            REGISTER = STACK.pop()
     elif command == ']':
         STACK.append(REGISTER)
     elif command == '{':
-        REGISTER = QUEUE.pop(0)
+        if len(QUEUE) == 0:
+            REGISTER = None
+        else:
+            REGISTER = QUEUE.pop(0)
     elif command == '}':
         QUEUE.append(REGISTER)
     elif command == '~':
@@ -101,21 +115,23 @@ def com(command, i):
         reg_val = REGISTER
         REGISTER = stack_upper
         STACK.append(reg_val)
+    elif command == "'":
+        REGISTER = None
     elif command == '(':
         if REGISTER is None:
             SKIP = True
-            return i
+            return (i, SKIP)
         IN_GOTO.append(i)
     elif command == ')':
         if SKIP:
             SKIP = False
-            return i
+            return (i, SKIP)
         if REGISTER is None:
             IN_GOTO.pop()
-            return i
+            return (i, SKIP)
         i = IN_GOTO[-1]
 
-    return i
+    return (i, SKIP)
 
 
 def operator_execute(operand):
@@ -148,19 +164,36 @@ def operator_execute(operand):
             REGISTER = None
 
 
-com_list = ['#', '_', '!', '.', '?', '[', ']', '{', '}', '~', '(', ')']
+com_list = ['#', '_', '!', '.', '?', '[', ']', '{', '}', '~', '(', ')', '"', "'"]
 operators_list = ['+', '-', '*', '/', '%', '<', '>', '=', '&', '|']
-'''sym: a-z 0-9 @(=space)
-func: ()'''
+func_com_list = ['^', '$', ':', ';']
+'''
+    sym: a-z 0-9 @(=space)
+    loop(jumper): ()
+    func: ^name - implementation, $name - call
+'''
 
 
-def interp(code):
-    global REGISTER, STACK, QUEUE, SKIP, STEP
-    i = 0
-    depth = 0
+def read_func_name(code, index):
+    func_name = ''
     while True:
-        sym = code[i]
-        if SKIP:
+        sym = code[index]
+        if sym in com_list or sym in operators_list or sym in func_com_list or sym.isdigit() or sym == '@':
+            break
+        func_name += sym
+        index += 1
+    return (func_name, index)
+
+
+def interp(code, index=0):
+    global REGISTER, STACK, QUEUE, STEP, FUNC_TABLE, IN_FUNC
+    SKIP = False
+    depth = 0
+    func_reading = False
+    while True:
+        sym = code[index]
+
+        if SKIP and not func_reading:
             if sym == '(':
                 depth += 1
             elif sym == ')':
@@ -168,28 +201,73 @@ def interp(code):
                 SKIP = bool(depth >= 0)
         else:
             if sym == '"':
-                break
+                break                
+
+            if sym == ';':
+                if IN_FUNC:
+                    return
+                elif not func_reading:
+                    InterpreterError(index, 'unexpected found ";" outside the end of the function declaration')
+                func_reading = False
+                index += 1
+                continue
+
+            if func_reading:
+                index += 1
+                continue
+
+            if sym == '$': # func call
+                func_name, index = read_func_name(code, index + 1)
+                if func_name not in FUNC_TABLE:
+                    InterpreterError(index, f'no such function named {func_name}')
+                IN_FUNC = True
+                interp(code, FUNC_TABLE[func_name])
+                IN_FUNC = False
+                continue
+
+            elif sym == '^': # reading the new func implementation
+                func_name, index = read_func_name(code, index + 1)
+
+                if code[index] != ':':
+                    bad_sym_index = len(func_name)
+                    while code[index] != ':':
+                        func_name += code[index]
+                        index += 1
+                    InterpreterError(index, f'unsuitable function name <{func_name}>\n' + '_' * (37 + bad_sym_index) + '^')
+                
+                if func_name in FUNC_TABLE:
+                    InterpreterError(index, f'redefinition of a function <{func_name}>')
+                
+                FUNC_TABLE[func_name] = index + 1
+                func_reading = True
+                index += 1
+                continue
+
             elif sym.isdigit():
                 REGISTER = int(sym)
+
             elif sym in com_list:
-                i = com(sym, i)
+                index, SKIP = com(sym, index, SKIP)
+
             elif sym in operators_list:
                 operator_execute(sym)
+
             elif sym == '@':
                 REGISTER = ' '
+
             else:
                 REGISTER = sym
         if STEP:
             print("\n---------------------------------")
-            print(code[i])
+            print(code[index])
             print("REGISTER: ", REGISTER if REGISTER is not None else '[BARMALEY]')
             print("STACK: ", STACK)
             print("QUEUE: ", QUEUE)
             input()
-        i += 1
+        index += 1
 
 def parse(file_name, file_input):
-    global stack_open, queue_open, register_open, INPUT_QUEUE
+    global stack_open, queue_open, register_open
     parsedcode = ""
     try:
         with open(file_name, 'r') as f:
@@ -197,9 +275,9 @@ def parse(file_name, file_input):
     except FileExistsError or FileNotFoundError:
         raise CodeFileError()
     
-    if '?' in parsedcode and file_input is None:
-        INPUT_QUEUE = input().split()
-        INPUT_QUEUE = [int(val) if val.isdigit() else val for val in INPUT_QUEUE]
+    #if '?' in parsedcode and file_input is None:
+    #    INPUT_QUEUE = input().split()
+    #    INPUT_QUEUE = [int(val) if val.isdigit() else val for val in INPUT_QUEUE]
     return parsedcode
 
 
@@ -234,6 +312,7 @@ def start(source=None, inp=None):
     parse_code = refactor(parse_code)
 
     interp(parse_code)
+    print()
 
     cout()
 
